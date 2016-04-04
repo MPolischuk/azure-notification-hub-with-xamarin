d13d77ff5ce07254919a89055dc45713babc043d
MPolischuk <marcospolischuk@gmail.com>
2016-04-04 11:39:27 -0300

    Añadidos proyectos de prueba

---
+++ AppAndroidTestHub/Constants.cs
@@ -0,0 +1,10 @@
+
+namespace AppAndroidTestHub
+{
+    public static class Constants
+    {
+        public const string SenderID = "<Google API Project Number>"; // Google API Project Number
+        public const string ListenConnectionString = "<ListenConnectionString>";
+        public const string NotificationHubName = "<HubName>";
+    }
+}
---
+++ TestPushSend/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Azure.NotificationHubs;
+
+namespace TestPushSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendNotificationAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendNotificationAsync()
+        {
+            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString(
+                "<ConnectionStringWithFullAccess>",
+                "<HubName>");
+            await hub.SendGcmNativeNotificationAsync("{ \"data\" : {\"message\":\"Probando Notificaciones desde Azure!\"}}");
+        }
+    }
+}
